05b4a1c97fbd7bb1f85076abddeefb49f7dc3471
Aleksander-Dementev <74053529+Aleksander-Dementev@users.noreply.github.com>
2020-11-11 21:40:58 +0300

    Update Main.java

--- Task 2.9./Main.java
+++ Task 2.9./Main.java
@@ -1,19 +1,18 @@
-//решено частично нужна помощь
-public class Task_2_9_ {
+//решено c помощию не знал что возможна запись length-1
+public class Task_2_9 {
 /*
 Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5.
  Если есть - выведите 'да', а если нет - выведите 'нет'.
 */
         public static void main(String[] args) {
-            int massivs[] = {2, 43, 5, 43, 14};
+            int massivs[] = {15, 5, 12, 43, 5};
+            int a = 5;
                         for (int i = 0; i < massivs.length; i++) {
-                /*if (massiv[i] == 5) System.out.println("да");
-                /*else if (massiv[i] != 5)System.out.println("нет");*/
-                            while ((massivs[i] == 5)) {
+                        if ((massivs[i] == a)) {
                                 System.out.println("да");
                                 break;
-                            }
-                            if (massivs[i] != 5) {
+                            }else
+                            if (i == massivs.length-1) {
                                 System.out.println("нет");
                             }
 }}}
