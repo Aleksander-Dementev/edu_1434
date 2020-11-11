e89707f9f7c0cc0e1a72746f6a393a2d10599e3e
Aleksander-Dementev <74053529+Aleksander-Dementev@users.noreply.github.com>
2020-11-11 17:43:44 +0300

    Create Main.java

---
+++ Task 2.9./Main.java
@@ -0,0 +1,22 @@
+//решено частично нужна помощь
+public class Task_2_9_ {
+/*
+Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5.
+ Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+        public static void main(String[] args) {
+            int massivs[] = {2, 43, 5, 43, 14};
+                        for (int i = 0; i < massivs.length; i++) {
+                /*if (massiv[i] == 5) System.out.println("да");
+                /*else if (massiv[i] != 5)System.out.println("нет");*/
+                            while ((massivs[i] == 5)) {
+                                System.out.println("да");
+                                break;
+                            }
+                            if (massivs[i] != 5) {
+                                System.out.println("нет");
+                            }
+}}}
+
+
+
