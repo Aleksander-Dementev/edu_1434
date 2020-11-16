2c6cfec1885ab6766f9c12d467104cefd30c09ff
Aleksander-Dementev <74053529+Aleksander-Dementev@users.noreply.github.com>
2020-11-16 17:10:59 +0300

    Create Main.java

---
+++ task 3.4/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа.
+Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+public class Task_3_4_ {
+    public static void main(String[] args) {
+        System.out.println(sum(5, 4));
+    }
+
+        public static boolean sum(int a, int b) {
+            return (a+b)<10;
+
+
+    }
+}
