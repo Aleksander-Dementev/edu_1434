fb76ab7fd87ad9b92ab3400a273bde285a01bc7b
Aleksander-Dementev <74053529+Aleksander-Dementev@users.noreply.github.com>
2020-11-16 16:52:15 +0300

    Create Main.java

---
+++ task 3.3/Main.java
@@ -0,0 +1,13 @@
+public class Task_3_3_ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите 1-е число: ");
+        String nom1 = scanner.nextLine();
+        System.out.print("Введите 2-е число: ");
+        String nom2 = scanner.nextLine();
+        System.out.println(return1(nom1,nom2);
+    }
+        public static boolean return1(int a, int b) {
+        return  a==b;
+    }
+}
