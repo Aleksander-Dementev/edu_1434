c9cb167ba0a9f6eaac96f8b49167e3a8b4f83f0a
Aleksander-Dementev <74053529+Aleksander-Dementev@users.noreply.github.com>
2020-12-07 19:26:37 +0300

    Update Main.java

--- task 2.8/Main.java
+++ task 2.8/Main.java
@@ -4,22 +4,16 @@ public class Task_2_8 {
 Задание: Дан массив с элементами [2, 3, 4, 5].
 С помощью цикла for найдите произведение элементов этого массива.
 */
-        public static void main(String[] args) {
-          int[] numbers = {2, 3, 4, 5, 1};
-        int sum = 0;
-        int i = 0;
-        while (sum <= 10) {
-            sum += numbers[i];
-            i++;
-            if (i == numbers.length && sum <= 10) {
-                System.out.println("Сумма всех числе массива меньше 10");
-                break;
-            }
-
+        public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 4, 5};
+        int mult = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            mult *= numbers[i];
 
         }
-        if (sum > 10) {
-            System.out.println(i);
-        }
+        System.out.println(mult);
+
+
     }
 }
